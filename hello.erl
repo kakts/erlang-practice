d6774dee3b8f1a5ceedab84a0dca2d33fde874be
kakts <akutsukeita@gmail.com>
2016-02-06 15:35:19 +0900

    Add hello2 function

--- hello.erl
+++ hello.erl
@@ -1,6 +1,6 @@
 
 -module(hello).
--export([add/2, hello/0, greet_and_add_two/1]).
+-export([add/2, hello/0, hello2/2, greet_and_add_two/1]).
 %% add A to B
 add(A, B) ->
 	A + B.
@@ -9,7 +9,14 @@ add(A, B) ->
 hello() ->
 	io:format("hello world~n").
 
-%%
 greet_and_add_two(X) ->
 	hello(),
 	add(X, 2).
+
+%%
+hello2(male, Name) ->
+	io:format("Hello, Mr. ~s!", [Name]); % for male
+hello2(female, Name) ->
+	io:format("Hello, Mrs. ~s!", [Name]); % for female
+hello2(_, Name) ->
+	io:format("Hello, ~s!", [Name]). % for others.
