5367b4addb55a55df4590f7c2a55404290c291b6
kakts <akutsukeita@gmail.com>
2016-02-06 15:37:51 +0900

    Refactor.

--- hello.erl
+++ hello.erl
@@ -1,22 +1,21 @@
-
 -module(hello).
 -export([add/2, hello/0, hello2/2, greet_and_add_two/1]).
 %% add A to B
 add(A, B) ->
-	A + B.
+  A + B.
 
 %% print out
 hello() ->
-	io:format("hello world~n").
+  io:format("hello world~n").
 
 greet_and_add_two(X) ->
-	hello(),
-	add(X, 2).
+  hello(),
+  add(X, 2).
 
 %%
 hello2(male, Name) ->
-	io:format("Hello, Mr. ~s!", [Name]); % for male
+  io:format("Hello, Mr. ~s!", [Name]); % for male
 hello2(female, Name) ->
-	io:format("Hello, Mrs. ~s!", [Name]); % for female
+  io:format("Hello, Mrs. ~s!", [Name]); % for female
 hello2(_, Name) ->
-	io:format("Hello, ~s!", [Name]). % for others.
+  io:format("Hello, ~s!", [Name]). % for others.
