e175eaf254bc664b312373eb7cabc32c5d6c51c7
kakts <akutsukeita@gmail.com>
2016-02-06 16:04:28 +0900

    Add validationTime.erl

---
+++ validationTime.erl
@@ -0,0 +1,9 @@
+-module(validationTime).
+-compile(export_all).
+
+valid_time({Date = {Y, M, D}, Time = {H, Min, S}}) ->
+  io:format("The Date tuple (~p) says today is: ~p/~p/~p,~n", [Date, Y, M, D]),
+  io:format("The time tuple (~p) indicates: ~p:~p:~p.~n", [Time, H, Min, S]);
+valid_time(_) ->
+  % 引数がformat通りでない場合
+  io:format("Stop feeding me wrong data! ~n").
